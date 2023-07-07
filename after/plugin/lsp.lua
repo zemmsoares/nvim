4a799c937f03690a08c02a3fdfba587b1a216051
zemmsoares <zemmsoares@gmail.com>
2023-07-07 18:46:39 +0100

    feat: :sparkles: Added base nvim config

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,12 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.ensure_installed({
+  'tsserver', 
+  'rust_analyzer',
+})
+
+lsp.setup()
---
+++ lua/zemmsoares/init.lua
@@ -0,0 +1,2 @@
+require("zemmsoares.set")
+require("zemmsoares.remap")
